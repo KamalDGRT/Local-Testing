99416ae5cb035a11777b67fc142e7a03bda7acb5
Kamal Sharma <kamaldgrt@gmail.com>
2020-02-13 17:41:54 +0530

    Create Empty Dashboard Folder with files

---
+++ Dashboard2/Empty Dashboard/assets/js/b_dashboard.js
@@ -0,0 +1,11 @@
+ jQuery(document).on('click', '.navbar-nav > .dropdown', function(e) {
+   e.stopPropagation();
+ });
+ $(".dropdown-submenu").click(function() {
+   $(".dropdown-submenu > .dropdown-menu").toggleClass("show");
+ });
+
+ $("#menu-toggle").click(function(e) {
+     e.preventDefault();
+     $("#wrapper").toggleClass("toggled");
+ });
